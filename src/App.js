abc90072ce9f9199debabc1fd3785200647b3dd0
yamanidev <elamine.yamani@gmail.com>
2022-03-28 20:11:18 +0100

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import RequireAuth from "./components/RequireAuth/RequireAuth";
 import LoginPage from "./pages/login/LoginPage";
+import NoMatchPage from "./pages/noMatch/NoMatchPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
-	return <LoginPage></LoginPage>;
+	return (
+		<Routes>
+			<Route path="/" element={<Navigate to="/login" />} />
+			<Route path="/login" element={<LoginPage />} />
+			<Route
+				path="/profile"
+				element={
+					<RequireAuth>
+						<ProfilePage />
+					</RequireAuth>
+				}
+			/>
+			<Route path="*" element={<NoMatchPage />} />
+		</Routes>
+	);
 }
 
 export default App;
