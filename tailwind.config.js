7de76dc38fa75bd51557c415842c20a08685f966
yamanidev <elamine.yamani@gmail.com>
2022-04-03 22:42:47 +0100

    Added Poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,25 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
+		fontFamily: {
+			sans: [
+				"Poppins",
+				"ui-sans-serif",
+				"system-ui",
+				"-apple-system",
+				"BlinkMacSystemFont",
+				"Segoe UI",
+				"Roboto",
+				"Helvetica Neue",
+				"Arial",
+				"Noto Sans",
+				"sans-serif",
+				"Apple Color Emoji",
+				"Segoe UI Emoji",
+				"Segoe UI Symbol",
+				"Noto Color Emoji",
+			],
+		},
 		extend: {},
 	},
 	plugins: [],
