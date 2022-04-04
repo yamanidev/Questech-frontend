5f37d6ff7e3282c15011ada25430dc29408bd96e
yamanidev <elamine.yamani@gmail.com>
2022-04-04 15:48:04 +0100

    Added routing for home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import RequireAuth from "./components/RequireAuth/RequireAuth";
+import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/login/LoginPage";
 import NoMatchPage from "./pages/noMatch/NoMatchPage";
 import ProfilePage from "./pages/profile/ProfilePage";
@@ -7,7 +8,7 @@ import ProfilePage from "./pages/profile/ProfilePage";
 function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<Navigate to="/login" />} />
+			<Route path="/" element={<HomePage />} />
 			<Route path="/login" element={<LoginPage />} />
 			<Route
 				path="/profile"
