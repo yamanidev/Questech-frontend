09a49de3d8d125dfc1e9192aa44218a7e9e60707
yamanidev <elamine.yamani@gmail.com>
2022-04-04 15:50:01 +0100

    Changed heading colors to primary blue

--- src/pages/noMatch/NoMatchPage.js
+++ src/pages/noMatch/NoMatchPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function NoMatchPage() {
-	return <div className="text-6xl text-white">404 Error</div>;
+	return <div className="text-6xl text-primary-blue">404 Error</div>;
 }
 
 export default NoMatchPage;
--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function ProfilePage() {
-	return <h1 className=" mt-20 text-5xl text-center text-white">Profile</h1>;
+	return <h1 className="mt-20 text-5xl text-center text-primary-blue">Profile</h1>;
 }
 
 export default ProfilePage;
