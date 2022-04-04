e4b1d5573e9d190dfeff59fc812b953536a6835c
yamanidev <elamine.yamani@gmail.com>
2022-04-04 01:37:45 +0100

    Redefined viewports and added some container config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1400px",
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: "1rem",
+			},
+		},
 		fontFamily: {
 			sans: [
 				"Poppins",
