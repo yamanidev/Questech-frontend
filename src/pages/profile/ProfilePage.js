7bbd8e6be39cf01d365787572049b8c09e1135c9
yamanidev <elamine.yamani@gmail.com>
2022-04-05 21:45:12 +0100

    Added navbar to profile page

--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import svg from "../../assets/notification-bell.svg";
 
 function ProfilePage() {
-	return <h1 className="mt-20 text-5xl text-center text-primary-blue">Profile</h1>;
+	return (
+		<>
+			<header className="border border-[#707070]">
+				<div className="container">
+					<nav className="py-3 flex items-center justify-between">
+						<div className="flex items-center justify-between">
+							<Link to="/" className="mr-11 text-xl font-black text-primary-blue">
+								Logo
+							</Link>
+							<Link to="/users">Users</Link>
+						</div>
+						<div className="w-28 flex items-center justify-between">
+							<img src={svg} alt="logo image" className="h-6 cursor-pointer" />
+							<div className="cursor-pointer border border-primary-blue rounded-full w-14 h-14"></div>
+						</div>
+					</nav>
+				</div>
+			</header>
+		</>
+	);
 }
 
 export default ProfilePage;
