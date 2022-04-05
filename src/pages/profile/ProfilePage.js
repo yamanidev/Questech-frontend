af782d3b74d1a042ac6b68a3d0a6a2e59c9be989
yamanidev <elamine.yamani@gmail.com>
2022-04-05 22:48:16 +0100

    Drop down menu added

--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import svg from "../../assets/notification-bell.svg";
 
 function ProfilePage() {
+	const [isProfileOpen, setIsProfileOpen] = useState(false);
+
 	return (
 		<>
 			<header className="border border-[#707070]">
@@ -14,13 +16,39 @@ function ProfilePage() {
 							</Link>
 							<Link to="/users">Users</Link>
 						</div>
-						<div className="w-28 flex items-center justify-between">
+						<div className="w-28 flex items-center justify-between relative">
 							<img src={svg} alt="logo image" className="h-6 cursor-pointer" />
-							<div className="cursor-pointer border border-primary-blue rounded-full w-14 h-14"></div>
+							<button
+								className="w-14 h-14 border border-primary-blue rounded-full cursor-pointer"
+								onClick={() => {
+									setIsProfileOpen(!isProfileOpen);
+								}}
+								onBlur={() => {
+									if (isProfileOpen) setIsProfileOpen(false);
+								}}></button>
+							{isProfileOpen && (
+								<div className="w-max px-2 absolute top-[4.2rem] right-0 bg-gray-100">
+									<button className="py-3">Log out</button>
+								</div>
+							)}
 						</div>
 					</nav>
 				</div>
 			</header>
+			<main>
+				<section className="pt-16">
+					<div className="container">
+						<div className="flex items-center gap-10">
+							<div className="border border-primary-blue rounded-full w-36 h-36"></div>
+							<div>
+								<h1 className="text-4xl font-black">John Doe</h1>
+								<p className="text-[#959191]">Administrator</p>
+								<p className="text-[#959191]">admin@admin.com</p>
+							</div>
+						</div>
+					</div>
+				</section>
+			</main>
 		</>
 	);
 }
