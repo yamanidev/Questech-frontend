a1c0ad448213c4b9f82680baa1b3e5fc3abd55ca
yamanidev <elamine.yamani@gmail.com>
2022-05-17 17:48:58 +0100

    Interpolation of user information

--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -1,14 +1,16 @@
 import React from "react";
+import userService from "../../services/user/user-service";
 
 function ProfilePage() {
+	const user = userService.getUser();
 	return (
 		<div className="container">
 			<div className="flex flex-col sm:flex-row items-center gap-10 text-center sm:text-left">
 				<div className="border border-primary-blue rounded-full w-36 h-36"></div>
 				<div>
-					<h1 className="text-4xl font-black">John Doe</h1>
-					<p className="text-[#959191]">Administrator</p>
-					<p className="text-[#959191]">admin@admin.com</p>
+					<h1 className="text-4xl font-black">{`${user.firstname} ${user.familyname}`}</h1>
+					<p className="text-[#959191]">{user.role}</p>
+					<p className="text-[#959191]">{user.email}</p>
 				</div>
 			</div>
 		</div>
