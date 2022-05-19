aef6fdb69426d8d9d07a29e7ff1009f6b14b1f84
yamanidev <elamine.yamani@gmail.com>
2022-05-19 09:29:20 +0100

    Settings page boilerplate

--- src/pages/settings/SettingsPage.js
+++ src/pages/settings/SettingsPage.js
@@ -3,7 +3,7 @@ import React from "react";
 function SettingsPage() {
 	return (
 		<div className="container">
-			<h1 className="mb-10 text-6xl font-semibold">Edit Teacher</h1>
+			<h1 className="mb-10 text-6xl font-semibold">Settings</h1>
 		</div>
 	);
 }
