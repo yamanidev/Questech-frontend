1592aebccc726a801c1e8d6154f7e88d29231c57
yamanidev <elamine.yamani@gmail.com>
2022-05-19 04:56:52 +0100

    Added pages for students and teachers

--- src/pages/admin/users/students/StudentsPage.js
+++ src/pages/admin/users/students/StudentsPage.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function StudentsPage() {
-	return <div className="container">StudentsPage</div>;
+	return (
+		<div className="container">
+			<h1 className="mb-10 text-6xl font-semibold">Students List</h1>
+		</div>
+	);
 }
 
 export default StudentsPage;
--- src/pages/admin/users/teachers/TeachersPage.js
+++ src/pages/admin/users/teachers/TeachersPage.js
@@ -1,7 +1,15 @@
 import React from "react";
+import TeachersTable from "../../../../components/TeachersTable/TeachersTable";
 
 function TeachersPage() {
-	return <div className="container">TeachersPage</div>;
+	return (
+		<div className="container">
+			<h1 className="mb-10 text-6xl font-semibold">Teachers List</h1>
+			<div className="h-[600px]">
+				<TeachersTable />
+			</div>
+		</div>
+	);
 }
 
 export default TeachersPage;
