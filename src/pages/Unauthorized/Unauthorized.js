0f4b45087d5ba1f6cfb849e7b896b8264ed52c63
yamanidev <elamine.yamani@gmail.com>
2022-05-21 00:51:59 +0100

    Starter version of Unauthorized page

---
+++ src/pages/Unauthorized/Unauthorized.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Unauthorized() {
+	return (
+		<div className="container">
+			<h1 className="mb-10 text-6xl font-semibold">Unauthorized</h1>
+		</div>
+	);
+}
+
+export default Unauthorized;
