52507a49cc00a501004e71d87ac155da4a41754c
yamanidev <elamine.yamani@gmail.com>
2022-05-21 16:46:37 +0100

    Starter version of LoadingSpinner component

---
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+
+function LoadingSpinner() {
+	return (
+		<div className="h-[400px] w-full grid place-items-center absolute">
+			<CircularProgress />
+		</div>
+	);
+}
+
+export default LoadingSpinner;
