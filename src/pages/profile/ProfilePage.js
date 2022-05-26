77c1a75d1dd8b9e1a35e5f7b59af26f286533648
yamanidev <elamine.yamani@gmail.com>
2022-05-26 23:03:40 +0100

    Added user profile placeholder image

--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -1,12 +1,19 @@
 import React from "react";
 import userService from "../../services/user/user-service";
+import userProfilePlaceholder from "../../assets/user-profile-placeholder.svg";
 
 function ProfilePage() {
 	const user = userService.getUser();
 	return (
 		<div className="container">
 			<div className="flex flex-col sm:flex-row items-center gap-10 text-center sm:text-left">
-				<div className="border border-primary-blue rounded-full w-36 h-36"></div>
+				<div className="border border-black rounded-full w-36 h-36">
+					<img
+						src={userProfilePlaceholder}
+						alt=""
+						className="w-full h-full rounded-full"
+					/>
+				</div>
 				<div>
 					<h1 className="text-4xl font-black">{`${user.firstname} ${user.familyname}`}</h1>
 					<p className="text-[#959191]">{user.role}</p>
