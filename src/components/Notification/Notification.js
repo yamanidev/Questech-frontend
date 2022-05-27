8cb692829c400d778d420b553c7161722617312f
yamanidev <elamine.yamani@gmail.com>
2022-05-27 11:45:13 +0100

    Starter version of Notification component

---
+++ src/components/Notification/Notification.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Alert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
+
+function Notification(props) {
+	return (
+		<Snackbar
+			open={props.open}
+			onClose={props.onClose}
+			anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+			autoHideDuration={3000}>
+			<Alert severity={props.type} variant="filled">
+				{props.message}
+			</Alert>
+		</Snackbar>
+	);
+}
+
+export default Notification;
