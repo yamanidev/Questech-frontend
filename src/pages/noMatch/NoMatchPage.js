c43cc19fd289d46c1addc756edf59c52b4b95f16
yamanidev <elamine.yamani@gmail.com>
2022-06-01 09:18:46 +0100

    404 page styled and populated

--- src/pages/noMatch/NoMatchPage.js
+++ src/pages/noMatch/NoMatchPage.js
@@ -1,7 +1,15 @@
 import React from "react";
+import error404 from "../../assets/404-error.svg";
 
 function NoMatchPage() {
-	return <div className="text-6xl text-primary-blue">404 Error</div>;
+	return (
+		<div className="container">
+			<img src={error404} alt="" className="w-full max-w-2xl mx-auto mt-20" />
+			<h1 className="mt-10 text-5xl font-medium text-center">
+				404 Error, page not found.
+			</h1>
+		</div>
+	);
 }
 
 export default NoMatchPage;
