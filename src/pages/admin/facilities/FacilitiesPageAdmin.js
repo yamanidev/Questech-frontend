086dc3607867788a3a54f703b6eb8e9d91043fb7
yamanidev <elamine.yamani@gmail.com>
2022-06-01 22:47:38 +0100

    FacilitiesTable added

--- src/pages/admin/facilities/FacilitiesPageAdmin.js
+++ src/pages/admin/facilities/FacilitiesPageAdmin.js
@@ -1,9 +1,13 @@
 import React from "react";
+import FacilitiesTable from "../../../components/admin/FacilitiesTable/FacilitiesTable";
 
 function FacilitiesPageAdmin() {
 	return (
 		<div className="container">
 			<h1 className="mb-10 text-6xl font-semibold">Facilities admin</h1>
+			<div className="h-[600px] w-full max-w-3xl mx-auto">
+				<FacilitiesTable />
+			</div>
 		</div>
 	);
 }
