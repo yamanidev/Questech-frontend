3c5667de6cbfd8506e01b4888df6705da001c63d
yamanidev <elamine.yamani@gmail.com>
2022-06-15 15:24:01 +0100

    Added delete option

--- src/components/admin/GroupsList/GroupsList.js
+++ src/components/admin/GroupsList/GroupsList.js
@@ -4,10 +4,17 @@ import GroupsListItem from "./GroupsListItem";
 function GroupsList(props) {
 	const { groups, level } = props;
 
+	if (groups.length === 0) return null;
+
 	return (
 		<div className="max-w-sm mx-auto lg:mx-0 py-2 flex flex-col rounded bg-gray-200 overflow-hidden">
 			{groups.map((group, index) => (
-				<GroupsListItem key={index} level={level} groupNumber={group.groupId.id} />
+				<GroupsListItem
+					key={index}
+					level={level}
+					groupNumber={group.groupId.id}
+					onDelete={props.onDelete}
+				/>
 			))}
 		</div>
 	);
--- src/components/admin/GroupsList/GroupsListItem.js
+++ src/components/admin/GroupsList/GroupsListItem.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function GroupsListItem(props) {
 	const { groupNumber, level } = props;
+
 	return (
-		<Link to={`/admin/${level}/group/${groupNumber}`} className="py-4 px-5 text-xl">
-			Group {groupNumber}
-		</Link>
+		<div className="py-4 px-5 flex justify-between text-xl">
+			<Link to={`/admin/${level}/group/${groupNumber}`} className="flex-1">
+				Group {groupNumber}
+			</Link>
+			<IconButton
+				onClick={() => {
+					props.onDelete(groupNumber);
+				}}>
+				<DeleteIcon />
+			</IconButton>
+		</div>
 	);
 }
 
