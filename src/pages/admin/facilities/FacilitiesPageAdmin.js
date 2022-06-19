322c0d51ee26a83e3cf23c25874938bfb004ecee
yamanidev <elamine.yamani@gmail.com>
2022-06-19 16:08:46 +0100

    Changed title

--- src/pages/admin/facilities/FacilitiesPageAdmin.js
+++ src/pages/admin/facilities/FacilitiesPageAdmin.js
@@ -4,7 +4,7 @@ import FacilitiesTable from "../../../components/admin/FacilitiesTable/Facilitie
 function FacilitiesPageAdmin() {
 	return (
 		<div className="container">
-			<h1 className="mb-10 text-6xl font-semibold">Facilities admin</h1>
+			<h1 className="mb-10 text-6xl font-semibold">Facilities</h1>
 			<div className="h-[600px] w-full max-w-3xl mx-auto">
 				<FacilitiesTable />
 			</div>
