a88f58d00c878848145a05113d5b5cc8de67f9cd
yamanidev <elamine.yamani@gmail.com>
2022-06-20 05:40:43 +0100

    Relative positioning to container for LoadingSpinner

--- src/pages/admin/facilities/FacilitiesPageAdmin.js
+++ src/pages/admin/facilities/FacilitiesPageAdmin.js
@@ -5,7 +5,7 @@ function FacilitiesPageAdmin() {
 	return (
 		<div className="container">
 			<h1 className="mb-10 text-6xl font-semibold">Facilities</h1>
-			<div className="h-[600px] w-full max-w-3xl mx-auto">
+			<div className="h-[600px] w-full max-w-3xl relative mx-auto">
 				<FacilitiesTable />
 			</div>
 		</div>
